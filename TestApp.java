af26260f8a5c12f58b05f4e64f8e7564a7389e0c
Mahesh Deshmukh <40640800+maheshdeshmukh22@users.noreply.github.com>
2018-06-27 23:28:24 +0530

    testing

---
+++ TestApp.java
@@ -0,0 +1,10 @@
+package com.hadoopbasics.learnings;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
